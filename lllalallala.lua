5db98c4b52662451002895be3aa3e464a332fc82
fdsmogk <121389294+fdsmogk@users.noreply.github.com>
2023-02-03 18:31:28 +0500

    Update lllalallala.lua

--- lllalallala.lua
+++ lllalallala.lua
@@ -7,3 +7,16 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/wgrijndsapmoi
 --Admin
 wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/sgzhdsxhsrt6745745joinijodnoizsd25/main/sdhdfjherajserk6346347cdzs", true))()
+
+--AntiLock
+getgenv().Sky = true 
+getgenv().SkyAmount = 100000000000000000000000000
+
+game:GetService("RunService").heartbeat:Connect(function()
+    if getgenv().Sky ~= false then 
+    local vel = game.Workspace.Players.kreepozi.HumanoidRootPart.Velocity
+    game.Workspace.Players.kreepozi.HumanoidRootPart.Velocity = Vector3.new(0,      getgenv().SkyAmount,0) 
+    game:GetService("RunService").RenderStepped:Wait()
+    game.Workspace.Players.kreepozi.HumanoidRootPart.Velocity = vel
+    end 
+end)
