591076a5ac085dde4d50a28b29797c84dfb1593f
fdsmogk <121389294+fdsmogk@users.noreply.github.com>
2023-02-20 16:24:41 +0500

    Update lllalallala.lua

--- lllalallala.lua
+++ lllalallala.lua
@@ -1,10 +1,12 @@
 --Main
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/aehtnpopoaoerjpofehnajpo225236/main/hreajettsrka4658846545969", true))()
-
+print("Module 1 UI loaded")
 --Logs
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/wgrijndsapmoioiesrdmvipso324326523523/main/sfasfdrhadfgsad3253467457sdfgdfshfdhjj", true))()
-
+print("Module 2 Script loaded")
 --Safe
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/hrehetrhjsftjhfj/main/hdjtsrfjtsfjxgsxfjskxfjskfjkfjfjg", true))()
-
+print("Module 3 Safe loaded")
 --Fun
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/fafadsftrhththththth/main/fasdfjrtsjsrposjrpto4324523523", true))()
+print("Module 4 SafeV2 loaded")
