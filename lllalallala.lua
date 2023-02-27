3e032dc5c35975d086d4ab02bc0df1f04446a5bc
fdsmogk <121389294+fdsmogk@users.noreply.github.com>
2023-02-27 12:52:06 +0500

    Update lllalallala.lua

--- lllalallala.lua
+++ lllalallala.lua
@@ -1,7 +1,5 @@
 --Main
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fdsmogk/aehtnpopoaoerjpofehnajpo225236/main/hreajettsrka4658846545969", true))()
-print("Module 1 UI loaded")
 --Commands
 _G.host = "KreepOZi"
 loadstring(game:HttpGet('https://raw.githubusercontent.com/uwuya/Eternals-Alt-Control/main/real'))()
-print("Module 3 Safe loaded")
